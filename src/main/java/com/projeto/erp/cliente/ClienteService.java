73532cb4638b652d184d6c7900eb1614d23265e1
Ailton Martins <pinho123@gmail.com>
2025-07-28 01:31:30 -0300

    feat(Cliente): Criado CRUD do Cliente

--- src/main/java/com/projeto/erp/cliente/ClienteService.java
+++ src/main/java/com/projeto/erp/cliente/ClienteService.java
@@ -1,5 +1,6 @@
 package com.projeto.erp.cliente;
 
+import com.projeto.erp.cliente.dto.ClienteRequestDTO;
 import com.projeto.erp.cliente.dto.ClienteResponseDTO;
 import com.projeto.erp.cliente.mapper.ClienteMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,4 +24,37 @@ public class ClienteService {
         List<ClienteResponseDTO> dtos = clientes.stream().map(mapper::toDTO).toList();
         return dtos;
      }
+
+    public ClienteResponseDTO getCliente( Long id ) {
+
+        var cliente = clienteRepository.findById(id).orElseThrow( () -> new RuntimeException("Cliente não encontrado com o ID:" + id)  );
+        ClienteResponseDTO dto = mapper.toDTO(cliente);
+        return dto;
+    }
+
+    public ClienteResponseDTO createCliente(ClienteRequestDTO cliente) {
+        Cliente newCliente = mapper.toEntity(cliente);
+        Cliente savedCliente = clienteRepository.save(newCliente);
+        return mapper.toDTO(savedCliente);
+    }
+
+    public ClienteResponseDTO updateCliente(Long id, ClienteRequestDTO cliente) {
+        Cliente existingCliente = clienteRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado com o ID: " + id));
+
+        existingCliente.setNome(cliente.getNome());
+        existingCliente.setEmail(cliente.getEmail());
+        existingCliente.setDocumento(cliente.getDocumento());
+        existingCliente.setTelefone(cliente.getTelefone());
+        existingCliente.setAtivo(cliente.getAtivo());
+
+        Cliente updatedCliente = clienteRepository.save(existingCliente);
+        return mapper.toDTO(updatedCliente);
+    }
+
+    public void deleteCliente(Long id) {
+        Cliente cliente = clienteRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado com o ID: " + id));
+        clienteRepository.delete(cliente);
+    }
 }
--- src/main/java/com/projeto/erp/cliente/mapper/ClienteMapper.java
+++ src/main/java/com/projeto/erp/cliente/mapper/ClienteMapper.java
@@ -1,18 +1,15 @@
 package com.projeto.erp.cliente.mapper;
 
 import com.projeto.erp.cliente.Cliente;
+import com.projeto.erp.cliente.dto.ClienteRequestDTO;
 import com.projeto.erp.cliente.dto.ClienteResponseDTO;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-import java.util.List;
-
-@Mapper
+@Mapper(componentModel = "spring")
 public interface ClienteMapper {
 
     ClienteMapper INSTANCE = Mappers.getMapper(ClienteMapper.class);
-
     ClienteResponseDTO toDTO(Cliente cliente);
-
+    Cliente toEntity(ClienteRequestDTO dto);
 }
